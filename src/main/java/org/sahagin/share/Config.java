e890c17638d71347052188d6febac5694e9d7a92
itonozomi <nosonosonoso@gmail.com>
2014-12-10 15:09:42 +0900

    Split config to “java” section and “common” section

--- src/main/java/org/sahagin/share/Config.java
+++ src/main/java/org/sahagin/share/Config.java
@@ -10,11 +10,13 @@ import org.sahagin.share.yaml.YamlConvertible;
 
 public class Config implements YamlConvertible {
     private static final String INVALID_CONFIG_YAML = "failed to load config file \"%s\": %s";
+    private static final File REPORT_INPUDT_DATA_DIR_DEFAULT = new File("sahagin-report-input");
+    private static final File REPORT_OUTPUDT_DATA_DIR_DEFAULT = new File("sahagin-report");
 
     private File rootDir;
     private File testDir;
-    private File reportInputDataDir;
-    private File reportOutputDir;
+    private File reportInputDataDir = REPORT_INPUDT_DATA_DIR_DEFAULT;
+    private File reportOutputDir = REPORT_OUTPUDT_DATA_DIR_DEFAULT;
     private boolean outputLog = false; // TODO provisional. this is only for debugging
 
     public static Config generateFromYamlConfig(File yamlConfigFile) throws YamlConvertException {
@@ -80,26 +82,43 @@ public class Config implements YamlConvertible {
 
     @Override
     public Map<String, Object> toYamlObject() {
-        Map<String, Object> result = new HashMap<String, Object>(16);
-        result.put("testDir", testDir.getPath());
-        result.put("reportInputDataDir", reportInputDataDir.getPath());
-        result.put("reportOutputDir", reportOutputDir.getPath());
-        result.put("outputLog", outputLog);
+        Map<String, Object> javaConf = new HashMap<String, Object>(4);
+        javaConf.put("testDir", testDir.getPath());
+        Map<String, Object> commonConf = new HashMap<String, Object>(4);
+        commonConf.put("reportInputDataDir", reportInputDataDir.getPath());
+        commonConf.put("reportOutputDir", reportOutputDir.getPath());
+        commonConf.put("outputLog", outputLog);
+        Map<String, Object> result = new HashMap<String, Object>(2);
+        result.put("java", javaConf);
+        result.put("common", commonConf);
         return result;
     }
 
     @Override
     public void fromYamlObject(Map<String, Object> yamlObject)
             throws YamlConvertException {
-        testDir = new File(YamlUtils.getStrValue(yamlObject, "testDir"));
-        reportInputDataDir = new File(YamlUtils.getStrValue(yamlObject, "reportInputDataDir"));
-        reportOutputDir = new File(YamlUtils.getStrValue(yamlObject, "reportOutputDir"));
-        // outputLog is not mandatory
-        Boolean outputLogObj = YamlUtils.getBooleanValue(yamlObject, "outputLog", true);
-        if (outputLogObj == null) {
-            outputLog = false;
-        } else {
-            outputLog = outputLogObj;
+        Map<String, Object> javaYamlObj = YamlUtils.getYamlObjectValue(yamlObject, "java");
+        testDir = new File(YamlUtils.getStrValue(javaYamlObj, "testDir"));
+
+        // common and it's child settings is not mandatory
+        reportInputDataDir = REPORT_INPUDT_DATA_DIR_DEFAULT;
+        reportOutputDir = REPORT_OUTPUDT_DATA_DIR_DEFAULT;
+        outputLog = false;
+        Map<String, Object> commonYamlObj = YamlUtils.getYamlObjectValue(yamlObject, "common", true);
+        if (commonYamlObj == null) {
+            return;
+        }
+        String reportInputDataDirValue = YamlUtils.getStrValue(commonYamlObj, "reportInputDataDir", true);
+        String reportOutputDirValue = YamlUtils.getStrValue(commonYamlObj, "reportOutputDir", true);
+        Boolean outputLogValue = YamlUtils.getBooleanValue(yamlObject, "outputLog", true);
+        if (reportInputDataDirValue != null) {
+            reportInputDataDir = new File(reportInputDataDirValue);
+        }
+        if (reportOutputDirValue != null) {
+            reportOutputDir = new File(reportOutputDirValue);
+        }
+        if (outputLogValue != null) {
+            outputLog = true;
         }
     }
 
