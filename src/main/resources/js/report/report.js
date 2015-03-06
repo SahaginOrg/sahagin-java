4d1506cfee933797b99d694d4376e5c2ee755a3b
NozomiIto <nozomi.ito@trident-qa.com>
2015-03-06 23:44:55 +0900

    Fix bad report layout on Firefox

--- src/main/resources/js/report/report.js
+++ src/main/resources/js/report/report.js
@@ -216,12 +216,12 @@ function adjustImageAreaSize() {
   var maxWidth = $("#right_container").width();
   var maxHeight;
   if (srcInfoShown) {
-    maxHeight = $("#right_container").height() - $("#outer_script_table_container").height() - 15;
+    maxHeight = $("#right_container").height() - $("#outer_script_table_container").height() - 20;
   } else {
-    maxHeight = $("#right_container").height() - 15;
+    maxHeight = $("#right_container").height() - 20;
   } 
   if (maxHeight <= 0) {
-    maxHeight = 15;
+    maxHeight = 20;
   }
   var areaSize = calcCaptureAreaSize(width, height, maxWidth, maxHeight);
   // need to change also li elements width to reflect width change immediately
